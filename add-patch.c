31043406d0b39aba595a7e7ff9fd441a40e0f908
Johannes Schindelin <johannes.schindelin@gmx.de>
2019-09-11 13:33:32 +0200

    fixup??? built-in add -i: start implementing the `patch` functionality in C
    Needed by `tg/stash-refresh-index`.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- add-patch.c
+++ add-patch.c
@@ -1545,6 +1545,7 @@ soft_increment:
 		strbuf_reset(&s->buf);
 		reassemble_patch(s, file_diff, 0, &s->buf);
 
+		discard_index(s->s.r->index);
 		if (s->mode->apply_for_checkout)
 			apply_for_checkout(s, &s->buf,
 					   s->mode->is_reverse);
@@ -1555,11 +1556,9 @@ soft_increment:
 					 NULL, 0, NULL, 0))
 				error(_("'git apply' failed"));
 		}
-		if (discard_index(s->s.r->index) < 0 ||
-		    repo_read_index_preload(s->s.r, NULL, 0) < 0)
-			return error(_("could not read index"));
-		repo_refresh_and_write_index(s->s.r, REFRESH_QUIET, 0, 1,
-					     NULL, NULL, NULL);
+		if (!repo_read_index(s->s.r))
+			repo_refresh_and_write_index(s->s.r, REFRESH_QUIET, 0,
+						     1, NULL, NULL, NULL);
 	}
 
 	putchar('\n');
@@ -1602,9 +1601,8 @@ int run_add_p(struct repository *r, enum add_p_mode mode,
 		s.mode = &patch_mode_stage;
 	s.revision = revision;
 
-	if (repo_read_index_preload(r, NULL, 0) < 0)
-		return error(_("could not read index"));
-	if (repo_refresh_and_write_index(r, REFRESH_QUIET, 0, 1,
+	if (discard_index(r->index) < 0 || repo_read_index(r) < 0 ||
+	    repo_refresh_and_write_index(r, REFRESH_QUIET, 0, 1,
 					 NULL, NULL, NULL) < 0 ||
 	    parse_diff(&s, ps) < 0) {
 		strbuf_release(&s.plain);
